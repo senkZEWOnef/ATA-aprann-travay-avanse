09e3a5e5a4d6f1ef18e86f5ba83d6b7272e45ea8
Ralph Ulysse <ralphulysse@Ralphs-MacBook-Air.local>
2025-10-12 18:55:38 -0400

    refactor(app): move to /app/[locale] and add localized layout

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,22 @@
+import "../globals.css";
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+
+export default async function RootLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: string };
+}) {
+  const messages = await getMessages();
+  return (
+    <html lang={locale}>
+      <body className="min-h-screen">
+        <NextIntlClientProvider messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
